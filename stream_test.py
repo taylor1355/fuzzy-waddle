57e4f15319b3f601d7fa9f6eeae739ce4f53232d
Tobias Fischer <tobiasfischer17@gmail.com>
2018-08-26 18:23:34 -0400

    Multi-threading

--- stream_test.py
+++ stream_test.py
@@ -2,19 +2,52 @@ import sys, time
 
 import utils
 
-from PySide2.QtCore import QTimer
-from PySide2.QtWidgets import QWidget, QPushButton, QLabel
-from PySide2.QtGui import QPixmap
+# from PySide2.QtCore import QTimer, QThread, QRunnable, QThreadPool
+# from PySide2.QtWidgets import QWidget, QPushButton, QLabel
+# from PySide2.QtGui import QPixmap
+
+from PySide2.QtGui import *
+from PySide2.QtWidgets import *
+from PySide2.QtCore import *
 
 class StreamTab(QWidget):
     def __init__(self):
         super(StreamTab, self).__init__()
-        openStreamButton = utils.createButton("Open Stream", self)
-        openStreamButton.released.connect(self._open_stream)
+        openStream = utils.createButton("Open Stream", self, 0, 0)
+        openStream.released.connect(self._open_stream)
+        closeStream = utils.createButton("Close Stream", self, 0, 1)
+        closeStream.released.connect(self._close_stream)
+
+        startWorker = utils.createButton("Start Worker", self, 0, 2)
+        startWorker.released.connect(self._start_worker)
+        stopWorker = utils.createButton("Stop Worker", self, 0, 3)
+        stopWorker.released.connect(self._stop_worker)
+
+        self.streamWindow = StreamWindow()
+        self.isWindowShown = False;
+        self.thread = StreamThread()
+
+        self.workerLabel = utils.createLabel("", self, 0, 4)
+        self.connect(self.thread, SIGNAL("send_back_qstring(QString)"), self._get_qstring)
+
+    def _get_qstring(self, text):
+        self.workerLabel.setText(text)
+        if self.isWindowShown == True:
+            self.streamWindow.setText(text + " has been passed through")
 
     def _open_stream(self):
-        self.streamWindow = StreamWindow()
+        self.isWindowShown = True;
         self.streamWindow.show()
+
+    def _close_stream(self):
+        self.isWindowShown = False;
+        self.streamWindow.hide()
+
+    def _start_worker(self):
+        self.thread.start()
+
+    def _stop_worker(self):
+        self.thread.terminate()
 
 
 class StreamWindow(QWidget):
@@ -26,14 +59,40 @@ class StreamWindow(QWidget):
         self.imageLabel = QLabel(self)
         self.imageLabel.setGeometry(0, 0, w, h)
 
-        self.updateTimer = QTimer()
-        self.updateTimer.timeout.connect(self._update_timer_tick);
-        self.updateTimer.start()
-
     def setImage(self, pixmap):
         self.imageLabel.setPixmap(pixmap)
 
-    def _update_timer_tick(self):
-        # update code here
-        print("in the updater")
-        time.sleep(1)
+    def setText(self, text):
+        self.imageLabel.setText(text)
+
+
+
+class StreamThread(QThread):
+    def __init__(self):
+        super(StreamThread, self).__init__()
+
+    def __del__(self):
+        self.wait()
+
+    def run(self):
+        while 1:
+            self.emit(SIGNAL("send_back_qstring(QString)"), "this is some text")
+            time.sleep(1)
+            self.emit(SIGNAL("send_back_qstring(QString)"), "this is some other text")
+            time.sleep(1)
+
+
+
+# class StreamWorker(QRunnable):
+#     def __init__(self):
+#         super(StreamWorker, self).__init__()
+#         self.running = False
+#
+#     def run(self):
+#         self.running = True
+#         while self.running == True:
+#             print("Doing work!")
+#             time.sleep(1)
+#
+#     def stop(self):
+#         self.running = False
--- utils.py
+++ utils.py
@@ -1,9 +1,21 @@
 import sys
 
-from PySide2.QtWidgets import QPushButton
+from PySide2.QtWidgets import QPushButton, QLabel
 
 def createButton(text, parent):
     button = QPushButton(text, parent)
     x, y, w, h = 10, 10, 120, 56
     button.setGeometry(x, y, w, h)
     return button
+
+def createButton(text, parent, x, y):
+    button = QPushButton(text, parent)
+    x, y, w, h = 10 + (x * 130), 10 + (y * 66), 120, 56
+    button.setGeometry(x, y, w, h)
+    return button
+
+def createLabel(text, parent, x, y):
+    label = QLabel(text, parent)
+    x, y, w, h = 10 + (x * 130), 10 + (y * 66), 120, 56
+    label.setGeometry(x, y, w, h)
+    return label
