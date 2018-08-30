2bfe24d83cbb48e492e57302839715ff447f6dd5
Tobias Fischer <tobiasfischer17@gmail.com>
2018-08-29 23:49:20 -0400

    Worked on Auto Fishing

--- auto_fishing_tab.py
+++ auto_fishing_tab.py
@@ -24,15 +24,19 @@ class AutoFishingTab(QWidget):
         self.thread = StreamThread()
 
     def _start_worker_button_handler(self):
+        print("starting worker")
         self.thread.start()
 
     def _stop_worker_button_handler(self):
+        print("stopping worker")
         self.thread.terminate()
 
 
-target_x = 574
-target_y = 222
-target_thresh = 3
+start_target_x = 574
+start_target_y = 222
+catch_target_x = 574
+catch_target_y = 205
+target_thresh = 5
 actions = True
 
 class StreamThread(QThread):
@@ -42,61 +46,88 @@ class StreamThread(QThread):
     def __del__(self):
         self.wait()
 
+    def getState(self): # idle=0, start=1, catch=2
+        self.window.move_to_foreground()
 
+        self.stream_img = self.window.grab_frame()
+        stream_gray = cv.cvtColor(self.stream_img, cv.COLOR_BGR2GRAY)
+
+        method = cv.TM_SQDIFF
+        result = cv.matchTemplate(self.comp_img, stream_gray, method)
+        mn, _, mnLoc, _ = cv.minMaxLoc(result)
+        MPx, MPy = mnLoc
+
+        status = 0
+
+        if (MPx > start_target_x - target_thresh and MPx < start_target_x + target_thresh and MPy > start_target_y - target_thresh and MPy < start_target_y + target_thresh):
+            return 1
+        elif (MPx > catch_target_x - target_thresh and MPx < catch_target_x + target_thresh and MPy > catch_target_y - target_thresh and MPy < catch_target_y + target_thresh):
+            return 2
+        else:
+            return 0
 
     def actions(self, action):
-        point = window.rect.center()
-        pyautogui.moveTo(point[0], point[1], duration=2)
-        sleep(1)
-
-        direct_input.PressKey("SPACE")
-        sleep(1)
-        direct_input.PressKey("SPACE")
+        if (action == 1):
+            direct_input.PressKey("SPACE")
+            print("casting line")
+            direct_input.ReleaseKey("SPACE")
+            time.sleep(5)
+        elif (action == 2):
+            direct_input.PressKey("SPACE")
+            print("reeling in")
+            direct_input.ReleaseKey("SPACE")
+            time.sleep(1.7)
+            direct_input.PressKey("SPACE")
+            print("playing game")
+            direct_input.ReleaseKey("SPACE")
+            time.sleep(10)
 
 
 
     def run(self):
-        window = GameWindow("BLACK DESERT")
-        window.move_to_foreground()
+        self.window = GameWindow("BLACK DESERT")
+        self.window.move_to_foreground()
 
-        method = cv.TM_SQDIFF
+        point = self.window.rect.center()
+        pyautogui.moveTo(point[0], point[1], duration=2)
 
         comp_path = "ref_images/fishing_space_bar.jpg"
-        comp_img = cv.imread(comp_path, 0)
+        self.comp_img = cv.imread(comp_path, 0)
 
-        stream_img = window.grab_frame()
-        stream_gray = cv.cvtColor(stream_img, cv.COLOR_BGR2GRAY)
+        while 1:
+            state = self.getState()
 
-        result = cv.matchTemplate(comp_img, stream_gray, method)
-        mn, _, mnLoc, _ = cv.minMaxLoc(result)
-        MPx, MPy = mnLoc
+            if (actions):
+                if (state == 1):
+                    # space bar
+                    self.actions(1)
+                elif (state == 2):
+                    self.actions(2)
 
-        status = 0 # idle=0, space_bar=1
+            else:
+                self.stream_img = self.window.grab_frame()
+                stream_gray = cv.cvtColor(self.stream_img, cv.COLOR_BGR2GRAY)
 
-        if (MPx > target_x - target_thresh and MPx < target_x + target_thresh and MPy > target_y - target_thresh and MPy < target_y + target_thresh):
-            status = 1
-        else:
-            status = 0
+                method = cv.TM_SQDIFF
+                result = cv.matchTemplate(self.comp_img, stream_gray, method)
+                mn, _, mnLoc, _ = cv.minMaxLoc(result)
+                MPx, MPy = mnLoc
+                trows,tcols = self.comp_img.shape[:2]
+                font = cv.FONT_HERSHEY_SIMPLEX
+                if (state == 0):
+                    # idle
+                    cv.putText(self.stream_img, "Idle Detected", (180, 25), font, 0.8, (255, 0, 0), 2, cv.LINE_AA)
+                elif (state == 1):
+                    # space bar
+                    cv.putText(self.stream_img, "Space Bar Detected", (180, 25), font, 0.8, (255, 0, 0), 2, cv.LINE_AA)
 
-        if (actions):
-            if (status == 0):
-                # idle
-                print("idle...")
-            elif (status == 1):
-                # space bar
-                self.actions(1)
+                print(str(MPx) + " " + str(MPy))
+                cv.rectangle(self.stream_img, (MPx, MPy), (MPx + tcols, MPy + trows), (0, 0, 255), 2)
+                cv.imshow("output", self.stream_img)
+                cv.waitKey(0)
+                cv.destroyAllWindows()
 
-        else:
-            trows,tcols = comp_img.shape[:2]
-            font = cv.FONT_HERSHEY_SIMPLEX
-            if (status == 0):
-                # idle
-                cv.putText(stream_img, "Idle Detected", (180, 25), font, 0.8, (255, 0, 0), 2, cv.LINE_AA)
-            elif (status == 1):
-                # space bar
-                cv.rectangle(stream_img, (MPx, MPy), (MPx + tcols, MPy + trows), (0, 0, 255), 2)
-                cv.putText(stream_img, "Space Bar Detected", (180, 25), font, 0.8, (255, 0, 0), 2, cv.LINE_AA)
+            print("sleeping...")
+            time.sleep(1)
 
-            cv.imshow("output", stream_img)
-            cv.waitKey(0)
-            cv.destroyAllWindows()
+        print("leaving")
